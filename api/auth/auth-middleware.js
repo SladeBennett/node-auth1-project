e499337bc2253a987c9f74b4befa9aa87fb4c32c
SladeBennett <slade8colt@gmail.com>
2024-09-06 18:33:20 -0600

    Complete except session error

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -8,8 +8,11 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  console.log('restricted')
-  next()
+  if (req.session.user) {
+    next()
+  } else {
+    next({ status: 401, message: 'You shall not pass!' })
+  }
 }
 
 /*
@@ -65,7 +68,7 @@ async function checkUsernameExists(req, res, next) {
 */
 function checkPasswordLength(req, res, next) {
   if (!req.body.password || req.body.password.length <= 3) {
-    next({ message: "Password must be longer than 3 chars", status: 422})
+    next({ message: "Password must be longer than 3 chars", status: 422 })
   } else {
     next()
   }
